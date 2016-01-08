75c239e4fa1a9b7e51116b314e468a1920cb9ea4
Natalie Polen <email": null>
2016-01-08 18:49:14 -0500

    Add next_question method in survey model; build post route for responses

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,10 +5,19 @@ get '/responses/new' do
   @survey_user = SurveysUser.new(user_id: user.id, survey_id: survey.id)
 
   if @survey_user.save
+    binding.pry
+    @next_question = survey.next_question
     erb :'responses/new'
   else
     # @errors = @survey_user.errors.full_messages
     # erb :'responses/new'
   end
 end
+
+post '/responses' do
+  response = Response.new(option_id: params[:option], surveys_user_id: params[:surveys_user])
+
+
+end
+
 
