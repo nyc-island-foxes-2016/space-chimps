5c341e1c32df6ccecc3172b0e8dd7011da5045a9
Ali Lyons & Jeff George <pair+arlyons.webdevjeffus@devbootcamp.com>
2016-01-09 14:20:08 -0500

    Implement and debug question-adding cycle in questions#new view and route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,27 @@
 get '/questions/new' do
+  binding.pry
   @survey = Survey.find_by(id: params[:survey_id])
+  @error = params[:error]
+
   erb :"/questions/new"
 end
 
 post '/questions' do
-  binding.pry
+  @question = Question.new(params[:question])
+
+  if @question.save
+
+    @options = []
+    content_string = params[:options][:content_string]
+
+    content_string.split(",").each do |option_content|
+      option_content.strip!
+      @options << Option.create(content: option_content, question: @question)
+    end
+
+    redirect "/questions/new?survey_id=#{@question.survey_id}"
+  else
+    redirect "/questions/new?survey_id=#{@survey.id}?error=Question did not save."
+  end
 
 end
