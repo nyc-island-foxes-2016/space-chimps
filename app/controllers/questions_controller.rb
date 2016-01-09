861034d734c3f73b9c7b4ddca4953705f1741439
Ali Lyons & Jeff George <pair+arlyons.webdevjeffus@devbootcamp.com>
2016-01-09 12:44:02 -0500

    Implement separation of created and available surveys on surveys/index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,9 @@
 get '/questions/new' do
-  binding.pry
   @survey = Survey.find_by(id: params[:survey_id])
   erb :"/questions/new"
 end
 
-# NEXT STEP: ADD Questions Post Route
+post '/questions' do
+  binding.pry
+
+end
