8a3fdf34a484066f626f1bc9811e248c51743750
Ali Lyons & Will Van Wart <pair+arlyons.williej@devbootcamp.com>
2016-01-08 19:15:14 -0500

    Implement survey/show, /new and /edit routes and views

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,45 @@
+get '/surveys' do
+  @surveys = Survey.all
+  erb :'/surveys/index'
+end
+
+get '/surveys/new' do
+  erb :"/surveys/new"
+end
+
+get '/surveys/:id' do
+  @survey = Survey.find_by(id: params[:id])
+  @user = @survey.user
+  erb :"/surveys/show"
+end
+
+post "/surveys" do
+  @survey = Survey.new(params[:survey])
+  @survey.user = current_user
+  if @survey.save
+    redirect "/surveys/#{@survey.id}"
+  else
+    @errors = @survey.errors.full_messages
+    erb :"/surveys/edit"
+  end
+end
+
+get '/surveys/:id/edit' do
+  @survey = Survey.find_by(id: params[:id])
+  erb :"/surveys/edit"
+end
+
+put '/surveys/:id' do
+  @survey = Survey.find_by(id: params[:id])
+  if @survey.update(params[:survey])
+    redirect "/surveys/#{@survey.id}"
+  else
+    @errors = @survey.errors.full_messages
+    erb :"/surveys/edit"
+  end
+end
+
+delete "/surveys/:id" do
+  Survey.find_by(id: params[:id]).destroy
+  redirect "/users/#{current_user.id}"
+end
