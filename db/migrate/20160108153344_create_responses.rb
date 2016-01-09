4bb2015686ee74193609d3116433013e1de775a0
Natalie Polen <email": null>
2016-01-08 20:16:29 -0500

    Edit response migration to include question_id

--- db/migrate/20160108153344_create_responses.rb
+++ db/migrate/20160108153344_create_responses.rb
@@ -2,6 +2,7 @@ class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
       t.integer :option_id, null: false, index: true
+      t.integer :question_id, null: false, index: true
       t.integer :surveys_user_id, null: false, index: true
 
       t.timestamps null: false
