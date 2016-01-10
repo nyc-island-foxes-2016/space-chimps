1a2af27084718476022687c6c4a7554f0c03d0a5
Ali Lyons & Jeff George <pair+arlyons.webdevjeffus@devbootcamp.com>
2016-01-10 17:30:36 -0500

    Remove all references to things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,5 @@
-get '/users' do
-  @users = User.all
-  erb :'/users/index'
-end
-
 get '/users/new' do
   erb :'/users/new'
-end
-
-get '/users/:id' do
-  @user = User.find_by(id: params[:id])
-  @things = @user.things
-  erb :"/users/show"
 end
 
 post '/users' do
